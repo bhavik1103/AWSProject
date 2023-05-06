9dda60502ac42845473dabab2782ed45b1552f61
bhavik1103 <58806843+bhavik1103@users.noreply.github.com>
2023-05-05 18:03:53 -0700

    Delete mern-ec-2-main directory

--- mern-ec-2-main/frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import goalSlice from "../features/goal/goalSlice";
-import authReducer from "./../features/auth/authSlice";
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    goal: goalSlice,
-  },
-});
